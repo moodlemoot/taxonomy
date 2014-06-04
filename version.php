a08da3cca560f8da233a3cd46dd024bff631b9b0
Guillaume Allegre <guillaume.allegre@silecs.info>
2014-06-04 16:49:24 +0200

    version.php init

---
+++ version.php
@@ -0,0 +1,15 @@
+<?php
+// This file is part of a plugin for Moodle - http://moodle.org/
+
+/**
+ * @package    local
+ * @subpackage taxonomy
+ * @copyright  2014 MoodleMoot-FR 2014 CodeSprint Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2014060400;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2014050100;        // Requires this Moodle version
+$plugin->component = 'local_taxonomy';       // Full name of the plugin (used for diagnostics)
