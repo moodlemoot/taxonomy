f13e15dc113ae720f47231918eb116f9793fd0ee
penot <emmanuel.penot@univ-tours.fr>
2014-06-05 10:24:43 +0200

    Update local_taxonomy.php

--- lang/en/local_taxonomy.php
+++ lang/en/local_taxonomy.php
@@ -1,2 +1,7 @@
 $string['pluginname'] = 'Taxonomy';
-
+$string['eventvocabularycreated'] = 'Vocabulaire crée';
+$string['eventvocabularydeleted'] = 'Vocabulaire supprimé';
+$string['eventvocabularyupdated'] = 'Vocabulaire modifié';
+$string['eventtermcreated'] = 'Terme crée';
+$string['eventtermdeleted'] = 'Terme supprimé';
+$string['eventtermupdated'] = 'Terme modifié';
