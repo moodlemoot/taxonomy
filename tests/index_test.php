effce1f09a9fcb3c54bab37efa9a1e1b967d36a3
pascal fautrero <pascal.fautrero@crdp.ac-versailles.fr>
2014-06-06 11:58:33 +0200

    start tests

---
+++ tests/index_test.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Unit tests
+ * 
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package taxonomy
+ */
+global $CFG;
+//if (!isset($plugin)) $plugin = new stdClass();
+//include($CFG->dirroot . '/course/format/page/version.php');
+
+class index_test extends advanced_testcase 
+{
+
+    function testDisplayIndex() 
+    {
+        global $USER;
+        $this->resetAfterTest(true);
+
+        $PAGE = new moodle_page();
+        $PAGE->set_context(context_system::instance());
+
+        /**
+         * Try to access 'Taxonomy Page' with authentication and edition activated
+         */
+        $this->setAdminUser();
+        $USER->editing = 1;
+        $_SERVER['REQUEST_URI'] = "/local/taxonomy/index.php";
+        $_SERVER['REQUEST_METHOD'] = "GET";
+        ob_start();
+        $front = frontController::getInstance()->dispatch();
+        $out = ob_get_contents();
+        ob_end_clean();
+        $this->assertContains("Vocabulaire", $out);
+
+        /**
+         * Try to access 'Add Page' without authentication
+         */
+        $this->setUser(null);
+        $USER->editing = 0;
+        $_SERVER['REQUEST_URI'] = "/local/taxonomy/index.php";
+        $_SERVER['REQUEST_METHOD'] = "GET";
+        ob_start();
+        $front = frontController::getInstance()->dispatch();
+        $out = ob_get_contents();
+        ob_end_clean();
+        $this->assertContains("page à accès restreint", $out);
+    }
+}
